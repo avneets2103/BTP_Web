4ae4f0588d76972d8a7b0e51760db7e27ec32139
sneha gupta <gsneha20012004@gmail.com>
2024-08-31 14:26:20 +0530

    chat interface

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,5 +1,10 @@
+'use client';
+import ChatInterface from "@/components/ChatInterface";
+
 export default function Home() {
   return(
-    <></>
+    <div>
+      <ChatInterface></ChatInterface>
+    </div>
   );
 }
