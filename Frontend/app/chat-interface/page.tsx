ce984bb74014c672b71937edb7458fc3f156d84d
sneha gupta <gsneha20012004@gmail.com>
2024-08-31 16:03:53 +0530

    made chat interface (doc chat interface)

--- Frontend/app/chat-interface/page.tsx
+++ Frontend/app/chat-interface/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ChatInterface from "@/my_components/chat-interface/ChatInterface";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,10 +7,11 @@ import "react-toastify/dist/ReactToastify.css";
 export default function Home() {
   const Router = useRouter();
   useEffect(() => {
-    Router.push("/notification");
+    // Router.push("/notification");
   })
   return (
     <>
+    <ChatInterface></ChatInterface>
     </>
   );
 }
